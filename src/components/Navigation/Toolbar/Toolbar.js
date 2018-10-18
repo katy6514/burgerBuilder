b4f48696fa2a3b3ab726326533ffa2ea2514d258
Katy Wyman <katherine.i.wyman@gmail.com>
2018-10-18 09:50:06 -0700

    first commit

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import classses from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
+const toolbar = props => (
+  <header className={classses.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classses.Logo}>
+      <Logo />
+    </div>
+    <nav className={classses.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
+
+export default toolbar;
